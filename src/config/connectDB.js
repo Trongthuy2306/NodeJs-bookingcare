b8e59dadf2a307a42782b3b78e69450f4497a2e4
Trongthuy2306 <thuynguyen.23062002@gmail.com>
2023-08-02 02:14:51 +0700

    add and use cors

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('trongthuy', 'root', '123456', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging: false
 });
 
 let connectDB = async () => {
