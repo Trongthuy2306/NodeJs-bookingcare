cdc936d702bb6ee69475c8cac2476bfc3dc8e81c
Trongthuy2306 <thuynguyen.23062002@gmail.com>
2023-08-06 16:19:58 +0700

    lay thong tin user tu database

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,7 +18,25 @@ let handleLogin = async (req, res) => {
         user: userData.user ? userData.user : {}
     })
 }
+
+let handleGetAllUsers = async (req, res) => {
+    let id = req.body.id; //all , id
+    if (!id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: "Missing required parameter!",
+            users: []
+        })
+    }
+    let users = await userService.getAllUsers(id);
+
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: "Ok",
+        users
+    })
+}
 module.exports = {
     handleLogin: handleLogin,
-
+    handleGetAllUsers: handleGetAllUsers,
 }
